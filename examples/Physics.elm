cfb9040ab421eee8be34431206736933a18d2c42
Evan Czaplicki <info@elm-lang.org>
2014-09-13 13:20:52 -0700

    Add more type signatures

--- examples/Physics.elm
+++ examples/Physics.elm
@@ -12,14 +12,27 @@ position = create .position (\f r -> { r | position <- f r.position })
 velocity : Focus { r | velocity:a } a
 velocity = create .velocity (\f r -> { r | velocity <- f r.velocity })
 
+type Point =
+    { x : Float
+    , y : Float
+    }
+
+type Object =
+    { position : Point
+    , velocity : Point
+    }
+
+object : Object
 object =
     { position = { x=3, y=4 }
     , velocity = { x=1, y=1 }
     }
 
+physics : Float -> Object -> Object
 physics dt object =
     object
       |> update (position => x) (\px -> px + object.velocity.x * dt)
       |> update (position => y) (\py -> py + object.velocity.y * dt)
 
+main : Element
 main = asText (physics 1 object)
