2e25a1c8eb612b33529b433fd893875810b726cb
Evan Czaplicki <info@elm-lang.org>
2014-10-03 14:27:06 -0700

    Improve documentation a bit

--- Focus.elm
+++ Focus.elm
@@ -10,12 +10,14 @@ be writing code like this:
 This means you could avoid writing record update syntax which would be messier.
 
 **Warning!** It is possible that the concept of a `Focus` is harmful to code
-quality in that it can help you to be lax with abstraction boundaries. By making
-it easy to look deep inside of data structures, it encourages you to stop
-thinking about how to make these substructures modular, perhaps leading to an
-architecture that is not as nice and has extra conceptual complexity. It may
-also make your code slower by encouraging you to take many passes over data,
+quality in that it can help you to be lax with abstraction boundaries.
+
+By making it easy to look deep inside of data structures, it encourages you to
+stop thinking about how to make these substructures modular, perhaps leading
+to messier architecture *and* some extra conceptual complexity. It may also
+make your code slower by encouraging you to take many passes over data,
 creating lots of intermediate data structures for no particular reason.
+*Use with these risk in mind!*
 
 # Get, Set, Update
 @docs get, set, update
@@ -42,9 +44,7 @@ create get update =
 
 {-| Get a small part of a big thing.
 
-    type X r = { r | x:Float }
-
-    x : Focus (X r) Float
+    x : Focus { record | x:a } a
 
     get x { x=3, y=4 } == 3
 
@@ -57,9 +57,7 @@ get focus big =
 
 {-| Set a small part of a big thing.
 
-    type X r = { r | x:Float }
-
-    x : Focus (X r) Float
+    x : Focus { record | x:a } a
 
     set x 42 { x=3, y=4 } == { x=42, y=4 }
 -}
@@ -69,24 +67,22 @@ set focus small big =
 
 {-| Update a small part of a big thing.
 
-    type X r = { r | x:Float }
-
-    x : Focus (X r) Float
+    x : Focus { record | x:a } a
 
     update x sqrt { x=9, y=10 } == { x=3, y=10 }
 
 This lets us chain updates without any special record syntax:
 
-    type Y r = { r | y:Float }
-
-    y : Focus (Y r) Float
+    x : Focus { record | x:a } a
+    y : Focus { record | y:a } a
 
     point
       |> update x sqrt
       |> update y sqrt
 
 The downside of this approach is that this means we take two passes over the
-record, whereas normal record syntax would only have required one.
+record, whereas normal record syntax would only have required one. It may be
+best to use a mix `Focus` and typical record updates to minimize traversals.
 -}
 update : Focus big small -> (small -> small) -> big -> big
 update focus f big =
@@ -99,10 +95,10 @@ foci. This means we can update a field deep inside some nested records. For
 example, perhaps we want to add one to `object.physics.velocity.x` or set it to
 zero.
 
-    physics  : Focus { r | physics:a } a
-    velocity : Focus { r | velocity:a } a
-    x        : Focus { r | x:a } a
-    y        : Focus { r | y:a } a
+    physics  : Focus { record | physics  : a } a
+    velocity : Focus { record | velocity : a } a
+    x        : Focus { record | x        : a } a
+    y        : Focus { record | y        : a } a
 
     update (physics => velocity => x) (\x -> x + 1) object
 
@@ -117,10 +113,11 @@ doing a lot of silly work if you start writing code like this:
       |> set (physics => velocity => y) 0
 
 It is pretty, but you pay for it in performance because you take two passes
-over `object` instead of one.
+over `object` instead of one. It may be best to do the last step with typical
+record updates so that this can be done in one pass.
 -}
 (=>) : Focus big medium -> Focus medium small -> Focus big small
-largerFocus => smallerFocus =
+(=>) largerFocus smallerFocus =
     { get big = smallerFocus.get (largerFocus.get big)
     , update f big = largerFocus.update (smallerFocus.update f) big
     }
